c3a83d94728956b06f21c129ace3b98a6056fcf9
Frida Ljungberg <frlj2501@student.miun.se>
2026-02-03 17:08:00 +0100

    Lagt till displayData för att skriva ut data

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 "use strict";
+let allData = [];
 
-document.addEventListener("DOMContentLoaded", loadData);
+document.addEventListener("DOMContentLoaded", async () => {
+    loadData();
+
+    //Händelselyssnare för sortering
+    document.querySelector("#sort-by-code").addEventListener
+
+});
 
 async function loadData() {
     const url = "https://webbutveckling.miun.se/files/ramschema.json";
@@ -10,9 +17,31 @@ async function loadData() {
         const response = await fetch(url);
         const data = await response.json();
 
-        console.table(data);
+        //Lagra globalt
+        allData = data;
+
+        displayData(data);
 
     } catch (error) {
         console.error("Fel " + error);
     }
 }
+
+function displayData(data) {
+    const dataListEl = document.querySelector("#course-table");
+    //Radera
+    dataListEl.innerHTML = ""
+
+    //Loopa genom och skriv ut
+    data.forEach(data => {
+        dataListEl.innerHTML += `
+        <div class="course-row">
+            <div class="code">${data.code}</div>
+            <div class="name">${data.coursename}</div>
+            <div class="progression"> ${data.progression}</div> 
+        </div>
+        `;
+    });
+}
+
+
