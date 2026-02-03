85e92ee28a999b3ce15661802354a7caa1ae74eb
Frida Ljungberg <frlj2501@student.miun.se>
2026-02-03 17:32:23 +0100

    Lagt till sortering + text i README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Laboration 2 - DT211G
+Syftet med denna laboration är att träna på att använda FetchAPI-ansrop med async/await och try/catch samt träna på att sortera och filtrera data.
+
+## Om
+Av Frida Ljungberg, 2026.
+
+### Länk till webbplats
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,28 @@ let allData = [];
 
 document.addEventListener("DOMContentLoaded", async () => {
     loadData();
+
+    //Händelselyssnare för sortering
+    document.querySelector("#sort-by-code").addEventListener("click", () => {
+        const sortedByCode = [...allData].sort((a, b) =>
+            a.code.localeCompare(b.code, { numeric: true, sensitivity: 'base' })
+        );
+        displayData(sortedByCode);
+    });
+
+    document.querySelector("#sort-by-name").addEventListener("click", () => {
+        const sortedByName = [...allData].sort((a, b) =>
+            a.coursename.localeCompare(b.coursename, { numeric: true, sensitivity: 'base' })
+        );
+        displayData(sortedByName);
+    });
+
+    document.querySelector("#sort-by-progression").addEventListener("click", () => {
+        const sortedByProgression = [...allData].sort((a, b) =>
+            a.progression.localeCompare(b.progression, { numeric: true, sensitivity: 'base' })
+        );
+        displayData(sortedByProgression);
+    });
 });
 
 async function loadData() {
